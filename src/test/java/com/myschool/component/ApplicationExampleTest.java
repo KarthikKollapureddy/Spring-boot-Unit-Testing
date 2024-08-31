8b899a3298540503ac306997e4fb1e7f6252c7c1
KarthikKollapureddy <karthiksasik2000@ggmail.com>
2024-08-31 22:45:59 +0530

    create basic test class.

---
+++ src/test/java/com/myschool/component/ApplicationExampleTest.java
@@ -0,0 +1,12 @@
+package com.myschool.component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationExampleTest {
+    @Test
+    void basicTest(){
+//        basic test
+    }
+}
